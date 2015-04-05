c41fee093d21a9bcadf8b6d78d75a3b3fae9317c
Josh Quintus <josh@dotalign.com>
2015-04-05 01:54:16 -0400

    Add simple dummy project to test Team City configurations

---
+++ AmazingApp/AmazingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AmazingApp
+{
+    public class Program
+    {
+        public static string Msg { get { return "Hello World"; } }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Msg);
+        }
+    }
+}
