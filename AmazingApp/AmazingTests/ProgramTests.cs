c035b5cffb16d630aad1111ea302fd1fd5d641f9
Josh Quintus <josh@dotalign.com>
2015-04-05 02:29:19 -0400

    Include missing files

---
+++ AmazingApp/AmazingTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using AmazingApp;
+using NUnit.Framework;
+
+namespace AmazingTests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Msg_IsHelloWorld()
+        {
+            Assert.AreEqual("Hello World", Program.Msg);
+        }
+
+        [Test]
+        public void Pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
